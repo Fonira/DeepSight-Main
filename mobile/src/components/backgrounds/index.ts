e9804615ac8c5432eb0cd39bcd375b3318bc44f7
Fonira <maximeleparc3@gmail.com>
2026-04-27 01:00:15 +0200

    feat(mobile): add SunflowerLayer mascot with sprite frame from engine v3

--- mobile/src/components/backgrounds/index.ts
+++ mobile/src/components/backgrounds/index.ts
@@ -1,3 +1,4 @@
 export { DoodleBackground } from "./DoodleBackground";
 export { AmbientLightLayer } from "./AmbientLightLayer";
+export { SunflowerLayer } from "./SunflowerLayer";
 export { default } from "./DoodleBackground";
